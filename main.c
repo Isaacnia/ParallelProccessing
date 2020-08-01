56384106c092ccaa93e6ce524c048dfbe5c5bcc2
Rasool <ssamany73@gmail.com>
2020-08-02 01:18:31 +0430

    Add DotProduct and ShowMatrix

--- main.c
+++ main.c
@@ -7,16 +7,10 @@ int main(){
     int B[ROWS][COLS];
     int C[ROWS][COLS];
     int row,col;
-    int axis = 1 ; 
-    int value = 0 ;
-    binit(A);
-    for(row=0; row<ROWS; row++){
-        for(col=0; col<COLS; col++){
-            printf("%d ",A[row][col]);
-        }
-        printf("\n");
-    }
+    RbInit(B);
+    RaInit(A);
+    RcInit(C);
+    DotProduct(A,B,C);
+
 return 0;
 }
-
-
--- tools.h
+++ tools.h
@@ -1,4 +1,4 @@
-void ainit(int A[ROWS][COLS]){
+void RaInit(int A[ROWS][COLS]){
     int row,col;
     int axis = 1 ; 
     int value = 0 ;
@@ -13,7 +13,7 @@ void ainit(int A[ROWS][COLS]){
         axis++ ;
     }
 }
-void binit(int A[ROWS][COLS]){
+void RbInit(int A[ROWS][COLS]){
     int row,col;
     int axis = 8 ; 
     int value = 0 ;
@@ -26,5 +26,34 @@ void binit(int A[ROWS][COLS]){
             A[row][col] = value ;
         }
         axis-- ;
+    }
+}
+
+void RcInit(int A[ROWS][COLS]){
+    int row,col;
+    for(row=0; row<ROWS; row++){
+        for(col=0; col<COLS; col++){
+            A[row][col] = 0 ;
+        }
+    }
+}
+
+void DotProduct(int A[ROWS][COLS] , int B[ROWS][COLS], int C[ROWS][COLS]){
+    int row,col,step;
+    for (row = 0; row < ROWS; row++){
+        for (col = 0; col < COLS; col++){
+            for(step = 0; step < ROWS; step++){
+                C[row][col] += A[row][step] * B[step][col];
+            }
+        }
+    }
+}
+
+void ShowMatrix(int X[ROWS][COLS]){
+    for(row=0; row<ROWS; row++){
+        for(col=0; col<COLS; col++){
+            printf("%d ",X[row][col]);
+        }
+    printf("\n");
     }
 }
